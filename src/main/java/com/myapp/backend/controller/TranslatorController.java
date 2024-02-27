5af7b8d987e4406b2cf75f1c039438fe24c1961a
PahanHvesco <81190728+PahanHvesco@users.noreply.github.com>
2024-02-27 11:56:59 +0300

    Commit

--- src/main/java/com/myapp/backend/controller/TranslatorController.java
+++ src/main/java/com/myapp/backend/controller/TranslatorController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Locale;
-
 @RestController
 public class TranslatorController {
     @Autowired
