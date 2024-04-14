8d1161d64d56e51a8ae5e68f2591f46a684a4bb2
PahanHvesco <81190728+PahanHvesco@users.noreply.github.com>
2024-04-14 13:25:01 +0300

    Commit

--- src/test/java/com/myapp/backend/controller/HistoryTranslationControllerTest.java
+++ src/test/java/com/myapp/backend/controller/HistoryTranslationControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
@@ -31,6 +32,20 @@ public class HistoryTranslationControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    public void testGetHistoryTranslationById() throws Exception {
+        long id = 1L;
+        HistoryTranslationDto expectedDto = new HistoryTranslationDto();
+        expectedDto.setId(id);
+
+        when(historyTranslationService.convertEntityToDto(any())).thenReturn(expectedDto);
+        when(historyTranslationService.getHistoryTranslationById(id)).thenReturn(new HistoryTranslation());
+
+        mockMvc.perform(get("/history/translation/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
+    }
 
     @Test
     public void testGetAllHistoryTranslations() throws Exception {
