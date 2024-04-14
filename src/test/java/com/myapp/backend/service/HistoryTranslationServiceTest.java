39e45423ad309a23fd1ce967323723b8fe1f7d93
PahanHvesco <81190728+PahanHvesco@users.noreply.github.com>
2024-04-14 21:27:01 +0300

    Commit

--- src/test/java/com/myapp/backend/service/HistoryTranslationServiceTest.java
+++ src/test/java/com/myapp/backend/service/HistoryTranslationServiceTest.java
@@ -3,6 +3,7 @@ package com.myapp.backend.service;
 import com.myapp.backend.component.SimpleCacheComponent;
 import com.myapp.backend.dto.HistoryTranslationDto;
 import com.myapp.backend.dto.TranslatorDto;
+import com.myapp.backend.exception.ResourceNotFoundException;
 import com.myapp.backend.mapper.HistoryTranslationMapper;
 import com.myapp.backend.model.HistoryTranslation;
 import com.myapp.backend.model.Translator;
@@ -22,9 +23,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class HistoryTranslationServiceTest {
-    @InjectMocks
-    private HistoryTranslationService historyTranslationService;
-
     @Mock
     private HistoryTranslationRepository historyTranslationRepository;
 
@@ -34,134 +32,164 @@ class HistoryTranslationServiceTest {
     @Mock
     private SimpleCacheComponent<HistoryTranslation> simpleCacheComponent;
 
+    @InjectMocks
+    private HistoryTranslationService historyTranslationService;
+
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testCreateHistoryTranslation() {
-        // Mocking
-        Translator translator = new Translator();
-        String target = "target";
-        String source = "source";
-        when(historyTranslationRepository.save(any())).thenReturn(new HistoryTranslation());
-
-        // Testing
-        historyTranslationService.createHistoryTranslation(translator, target, source);
-
-        // Verification
-        verify(historyTranslationRepository, times(1)).save(any());
-    }
-
-    @Test
-    public void testGetAllHistoryTranslationsDto() {
-        // Mocking
-        List<HistoryTranslation> historyTranslations = new ArrayList<>();
-        historyTranslations.add(new HistoryTranslation());
-        when(historyTranslationRepository.findAll()).thenReturn(historyTranslations);
-        when(historyTranslationMapper.entityToDto(any())).thenReturn(new HistoryTranslationDto());
-
-        // Testing
-        List<HistoryTranslationDto> result = historyTranslationService.getAllHistoryTranslationsDto();
-
-        // Verification
-        assertNotNull(result);
-        assertFalse(result.isEmpty());
-    }
-
-    @Test
-    public void testGetHistoryTranslationById() {
-        // Mocking
-        long id = 1L;
+    void testCreateHistoryTranslation() {
+        // Устанавливаем заглушки для методов репозитория
         HistoryTranslation historyTranslation = new HistoryTranslation();
-        historyTranslation.setId(id);
-        when(simpleCacheComponent.get(id)).thenReturn(null);
-        when(historyTranslationRepository.existsById(id)).thenReturn(true);
-        when(historyTranslationRepository.findById(id)).thenReturn(Optional.of(historyTranslation));
-
-        // Testing
-        HistoryTranslation result = historyTranslationService.getHistoryTranslationById(id);
-
-        // Verification
-        assertNotNull(result);
-        assertEquals(id, result.getId());
-    }
-
-    @Test
-    public void testGetTranslationsBySourceAndTarget() {
-        // Mocking
-        String source = "source";
-        String target = "target";
-        List<TranslatorDto> translatorDtos = new ArrayList<>();
-        when(historyTranslationRepository.getTranslationsBySourceAndTarget(source, target)).thenReturn(translatorDtos);
-
-        // Testing
-        List<TranslatorDto> result = historyTranslationService.getTranslationsBySourceAndTarget(source, target);
-
-        // Verification
-        assertNotNull(result);
-        assertEquals(translatorDtos, result);
-    }
-
-    @Test
-    public void testGetAllHistoryTranslations() {
-        // Mocking
-        List<HistoryTranslation> historyTranslations = new ArrayList<>();
-        historyTranslations.add(new HistoryTranslation());
-        when(historyTranslationRepository.findAll()).thenReturn(historyTranslations);
-
-        // Testing
-        List<HistoryTranslation> result = historyTranslationService.getAllHistoryTranslations();
-
-        // Verification
-        assertNotNull(result);
-        assertFalse(result.isEmpty());
-    }
-
-    @Test
-    public void testDeleteHistoryTranslationById() {
-        // Mocking
-        long id = 1L;
-        when(historyTranslationRepository.existsById(id)).thenReturn(true);
-
-        // Testing
-        historyTranslationService.deleteHistoryTranslationById(id);
-
-        // Verification
-        verify(simpleCacheComponent, times(1)).remove(id);
-        verify(historyTranslationRepository, times(1)).deleteById(id);
-    }
-
-    @Test
-    public void testUpdateHistoryTranslation() {
-        // Mocking
-        long id = 1L;
-        HistoryTranslation updatedTranslation = new HistoryTranslation();
-        when(historyTranslationRepository.existsById(id)).thenReturn(true);
-        when(historyTranslationRepository.findById(id)).thenReturn(Optional.of(new HistoryTranslation()));
-
-        // Testing
-        HistoryTranslation result = historyTranslationService.updateHistoryTranslation(id, updatedTranslation);
-
-        // Verification
-        assertNull(result); // Assuming that this method returns null
-    }
-
-    @Test
-    public void testCreateHistoryTranslationsBulk() {
-        // Mocking
-        List<HistoryTranslation> historyTranslations = new ArrayList<>();
-        HistoryTranslation historyTranslation = new HistoryTranslation();
-        historyTranslations.add(historyTranslation);
         when(historyTranslationRepository.save(historyTranslation)).thenReturn(historyTranslation);
 
-        // Testing
-        historyTranslationService.createHistoryTranslationsBulk(historyTranslations);
+        // Вызываем метод сервиса
+        HistoryTranslation result = historyTranslationService.createHistoryTranslation(historyTranslation);
 
-        // Verification
+        // Проверяем, что метод репозитория был вызван один раз с правильным аргументом
         verify(historyTranslationRepository, times(1)).save(historyTranslation);
+
+        // Проверяем, что возвращенный результат равен ожидаемому объекту
+        assertEquals(historyTranslation, result);
     }
+
+    @Test
+    void testGetHistoryTranslationById_foundInCache() {
+        // Создаем объект HistoryTranslation и устанавливаем заглушку для метода get у SimpleCacheComponent
+        HistoryTranslation historyTranslation = new HistoryTranslation();
+        historyTranslation.setId(1L);
+        when(simpleCacheComponent.get(1L)).thenReturn(historyTranslation);
+
+        // Вызываем метод сервиса
+        HistoryTranslation result = historyTranslationService.getHistoryTranslationById(1L);
+
+        // Проверяем, что метод get у SimpleCacheComponent был вызван один раз с правильным аргументом
+        verify(simpleCacheComponent, times(1)).get(1L);
+
+        // Проверяем, что репозиторий не вызывался
+        verifyNoInteractions(historyTranslationRepository);
+
+        // Проверяем, что возвращенный результат равен ожидаемому объекту
+        assertEquals(historyTranslation, result);
+    }
+
+    @Test
+    void testGetHistoryTranslationById_notFoundInCache_foundInDB() {
+        // Устанавливаем заглушки для методов existsById и findById у HistoryTranslationRepository
+        when(simpleCacheComponent.get(1L)).thenReturn(null);
+        when(historyTranslationRepository.existsById(1L)).thenReturn(true);
+
+        // Создаем объект HistoryTranslation и устанавливаем заглушку для метода findById у HistoryTranslationRepository
+        HistoryTranslation historyTranslation = new HistoryTranslation();
+        when(historyTranslationRepository.findById(1L)).thenReturn(Optional.of(historyTranslation));
+
+        // Вызываем метод сервиса
+        HistoryTranslation result = historyTranslationService.getHistoryTranslationById(1L);
+
+        // Проверяем, что метод existsById и findById у HistoryTranslationRepository были вызваны один раз с правильными аргументами
+        verify(historyTranslationRepository, times(1)).existsById(1L);
+        verify(historyTranslationRepository, times(1)).findById(1L);
+
+        // Проверяем, что метод put у SimpleCacheComponent был вызван один раз с правильными аргументами
+        verify(simpleCacheComponent, times(1)).put(1L, historyTranslation);
+
+        // Проверяем, что возвращенный результат равен ожидаемому объекту
+        assertEquals(historyTranslation, result);
+    }
+
+    @Test
+    void testGetHistoryTranslationById_notFoundInCache_notFoundInDB() {
+        // Устанавливаем заглушки для методов existsById и findById у HistoryTranslationRepository
+        when(simpleCacheComponent.get(1L)).thenReturn(null); // Подменяем заглушку, чтобы вернуть null
+        when(historyTranslationRepository.existsById(1L)).thenReturn(false);
+
+        // Вызываем метод сервиса и проверяем, что выбрасывается исключение ResourceNotFoundException
+        assertThrows(ResourceNotFoundException.class, () -> historyTranslationService.getHistoryTranslationById(1L));
+
+        // Проверяем, что метод existsById у HistoryTranslationRepository был вызван один раз с правильным аргументом
+        verify(historyTranslationRepository, times(1)).existsById(1L);
+
+        // Проверяем, что метод findById у HistoryTranslationRepository не вызывался
+        verifyNoMoreInteractions(historyTranslationRepository);
+
+        // Проверяем, что метод get у SimpleCacheComponent был вызван один раз с правильным аргументом
+        verify(simpleCacheComponent, times(1)).get(1L);
+    }
+
+    @Test
+    void testGetTranslationsBySourceAndTarget() {
+        String source = "English";
+        String target = "Russian";
+
+        // Устанавливаем заглушку для метода getTranslationsBySourceAndTarget у HistoryTranslationRepository
+        List<TranslatorDto> expectedTranslators = new ArrayList<>();
+        when(historyTranslationRepository.getTranslationsBySourceAndTarget(source, target)).thenReturn(expectedTranslators);
+
+        // Вызываем метод сервиса
+        List<TranslatorDto> result = historyTranslationService.getTranslationsBySourceAndTarget(source, target);
+
+        // Проверяем, что метод getTranslationsBySourceAndTarget у HistoryTranslationRepository был вызван один раз с правильными аргументами
+        verify(historyTranslationRepository, times(1)).getTranslationsBySourceAndTarget(source, target);
+
+        // Проверяем, что возвращенный результат равен ожидаемому списку
+        assertEquals(expectedTranslators, result);
+    }
+
+    @Test
+    void testGetAllHistoryTranslations() {
+        // Устанавливаем заглушку для метода findAll у HistoryTranslationRepository
+        List<HistoryTranslation> expectedHistoryTranslations = new ArrayList<>();
+        when(historyTranslationRepository.findAll()).thenReturn(expectedHistoryTranslations);
+
+        // Вызываем метод сервиса
+        List<HistoryTranslation> result = historyTranslationService.getAllHistoryTranslations();
+
+        // Проверяем, что метод findAll у HistoryTranslationRepository был вызван один раз
+        verify(historyTranslationRepository, times(1)).findAll();
+
+        // Проверяем, что возвращенный результат равен ожидаемому списку
+        assertEquals(expectedHistoryTranslations, result);
+    }
+
+    @Test
+    void testGetAllHistoryTranslationsDto() {
+        // Создаем список объектов HistoryTranslation и устанавливаем заглушку для метода findAll у HistoryTranslationRepository
+        List<HistoryTranslation> historyTranslations = new ArrayList<>();
+        historyTranslations.add(new HistoryTranslation());
+        historyTranslations.add(new HistoryTranslation());
+        historyTranslations.add(new HistoryTranslation());
+        when(historyTranslationRepository.findAll()).thenReturn(historyTranslations);
+
+        // Создаем список объектов HistoryTranslationDto и устанавливаем заглушку для метода entityToDto у HistoryTranslationMapper
+        List<HistoryTranslationDto> expectedHistoryTranslationsDto = new ArrayList<>();
+        HistoryTranslationDto dto1 = new HistoryTranslationDto();
+        HistoryTranslationDto dto2 = new HistoryTranslationDto();
+        HistoryTranslationDto dto3 = new HistoryTranslationDto();
+        when(historyTranslationMapper.entityToDto(historyTranslations.get(0))).thenReturn(dto1);
+        when(historyTranslationMapper.entityToDto(historyTranslations.get(1))).thenReturn(dto2);
+        when(historyTranslationMapper.entityToDto(historyTranslations.get(2))).thenReturn(dto3);
+        expectedHistoryTranslationsDto.add(dto1);
+        expectedHistoryTranslationsDto.add(dto2);
+        expectedHistoryTranslationsDto.add(dto3);
+
+        // Вызываем метод сервиса
+        List<HistoryTranslationDto> result = historyTranslationService.getAllHistoryTranslationsDto();
+
+        // Проверяем, что метод findAll у HistoryTranslationRepository был вызван один раз
+        verify(historyTranslationRepository, times(1)).findAll();
+
+        // Проверяем, что метод entityToDto у HistoryTranslationMapper был вызван три раза с конкретными аргументами
+        verify(historyTranslationMapper, times(1)).entityToDto(historyTranslations.get(0));
+        verify(historyTranslationMapper, times(1)).entityToDto(historyTranslations.get(1));
+        verify(historyTranslationMapper, times(1)).entityToDto(historyTranslations.get(2));
+
+        // Проверяем, что возвращенный результат равен ожидаемому списку
+        assertEquals(expectedHistoryTranslationsDto, result);
+    }
+
 
     @Test
     public void testConvertEntityToDto() {
